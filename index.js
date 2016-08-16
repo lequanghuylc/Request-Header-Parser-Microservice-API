4790cfe073f0967a927d427a3fd347447c659935
quanghuyf <quanghuy@first-college.vn>
2016-08-16 20:05:31 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var requestIp = require('request-ip');
+app.enable('trust proxy');
+app.get("*", function(req, res){
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    var userAgent = req.headers['user-agent'];
+    var software = userAgent.substring(userAgent.indexOf("(")+1, userAgent.indexOf(")"));
+    var lang = req.headers['accept-language'];
+    var obj = {
+        "ipaddress": requestIp.getClientIp(req),
+        "language": lang.split(",")[0],
+        "software": software
+    };
+    res.end(JSON.stringify(obj));
+});
+
+app.listen(8080);
